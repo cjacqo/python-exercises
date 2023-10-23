45ca0b9c58292b5e45faca558d49ebdb416bedae
cjacqo <76092910+cjacqo@users.noreply.github.com>
2023-10-23 12:40:01 -0700

    Completed practice task 3

---
+++ Exercise 1.3/1.3-Practice Task 3/for_while_loops.py
@@ -0,0 +1,19 @@
+num = 10
+
+for i in range(1, 6):
+  print(int(num))
+  if (num == 50):
+    print("And we're done!")
+    break
+  else:
+    num = num + 10
+
+num = 10
+
+while num <= 50:
+  print(int(num))
+  if (num == 50):
+    print("And we're done!")
+    break
+  else:
+    num = num + 10
